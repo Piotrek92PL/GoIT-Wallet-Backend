8d44514543129a3ab8b8ddfcd050327947743d68
Annaerith <111195576+Joanna-Jasinska@users.noreply.github.com>
2023-12-01 09:30:16 +0100

    transaction - Joi validation

--- src/validators/transaction.js
+++ src/validators/transaction.js
@@ -1,14 +1,7 @@
 import Joi from "joi";
 export const contactSchema = Joi.object({
-  name: Joi.string().min(3).max(30).required(),
-  phone: Joi.string().min(1),
-  email: Joi.string().email({
-    minDomainSegments: 2,
-    tlds: { allow: ["com", "net", "pl"] },
-  }),
-  favorite: Joi.bool(),
-});
-
-export const favoriteSchema = Joi.object({
-  favorite: Joi.bool().required(),
+  type: Joi.string().valid("income", "expense").required(),
+  category: Joi.string(), //.required(), tutaj potrzeba konkretne kategorie trzymane w mongo
+  comment: Joi.string(),
+  date: Joi.date(),
 });
