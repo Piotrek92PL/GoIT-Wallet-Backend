615ca2aaa5d9405a0d9720ce2d9570fb9c8e9a31
Piotrek92PL <piotrb1992@gmail.com>
2023-12-07 17:56:48 +0100

    App.js update

--- src/app.js
+++ src/app.js
@@ -1,41 +1,40 @@
-import express from "express";
+import express from 'express';
 // import logger from "morgan";
-import cors from "cors";
-import swaggerUi from "swagger-ui-express";
-import swaggerJsdoc from "swagger-jsdoc";
-import transactionRouter from "#routes/transactionRoutes.js";
-import categoryRouter from "#routes/categoryRoutes.js";
-import dotenv from "dotenv";
-import "#config/config-passport.js";
-import routerUsers from "#routes/routerUsers.js";
+import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJsdoc from 'swagger-jsdoc';
+import transactionRouter from '#routes/transactionRoutes.js';
+import categoryRouter from '#routes/categoryRoutes.js';
+import dotenv from 'dotenv';
+import '#config/config-passport.js';
+import routerUsers from '#routes/routerUsers.js';
 
 const app = express();
 dotenv.config();
 
-// const formatsLogger = app.get("env") === "development" ? "dev" : "short";
-
 // app.use(logger(formatsLogger));
+app.use(express.urlencoded({ extended: true })); // dodane
 app.use(cors());
 app.use(express.json());
 
 const options = {
   definition: {
-    openapi: "3.0.0",
+    openapi: '3.0.0',
     info: {
-      title: "GoIT Wallet by FinanSync Team",
-      version: "1.0.0",
-      description: "Personal budget app",
+      title: 'GoIT Wallet by FinanSync Team',
+      version: '1.0.0',
+      description: 'Personal budget app',
     },
   },
-  apis: ["./routes/*.js"],
+  apis: ['./routes/*.js'],
 };
 
 const swaggerSpec = swaggerJsdoc(options);
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec)); //shows Swagger documentation
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec)); //shows Swagger documentation
 
-app.use("/api/transactions", transactionRouter);
-app.use("/api/category", categoryRouter);
-app.use("/api/users", routerUsers);
+app.use('/api/transactions', transactionRouter);
+app.use('/api/category', categoryRouter);
+app.use('/api/users', routerUsers);
 
 // Error handling
 
