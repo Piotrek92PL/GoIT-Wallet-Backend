a0885a9d318979424ca64e4a174185ecfb28fea1
Paweł Szopiński <szopinskipawel@gmail.com>
2023-12-09 23:26:27 +0100

    Swagger update

--- src/routes/categoryRoutes.js
+++ src/routes/categoryRoutes.js
@@ -3,14 +3,29 @@ import * as categoriesController from "#controllers/categories/index.js";
 const router = express.Router();
 
 /**
- * @swagger
+  * @swagger
  * /api/transactions:
  *   get:
  *     summary: Get all categories of transactions.
- *     description: Returns a list of all categories. Also creates default list in mongodb if there was no list of categories.
+ *     description: Returns a list of all categories. Also creates a default list in MongoDB if there was no list of categories.
  *     responses:
  *       200:
  *         description: A list of categories.
+ *         content:
+ *           application/json:
+ *             example:
+ *               status: 200
+ *               data:
+ *                 - category1
+ *                 - category2
+ *                 - category3
+ *       500:
+ *         description: Internal server error.
+ *         content:
+ *           application/json:
+ *             example:
+ *               status: 500
+ *               message: Internal server error: <error_message>
  */
 router.get("/", categoriesController.getAll);
 
